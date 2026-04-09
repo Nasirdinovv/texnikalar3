070a0ab50e06e8dcea6d6f488638d762b1f6ff60
Nasriddinovv Asadulloh <doniyornasirdinov82@gmail.com>
2026-04-10 00:39:30 +0500

    1234

--- ombor/urls.py
+++ ombor/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import all_products, product_detail
+from .views import all_products, product_detail, delete_product, update_product
 
 urlpatterns = [
     path('', all_products, name='all_products'),
     path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('delete/<int:id>/', delete_product, name='delete_product'),
+    path('uptade/delete/<int:id>/', update_product, name='update_product'),
+    
 ]
--- ombor/views.py
+++ ombor/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 
 
@@ -9,3 +9,32 @@ def all_products(request):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'product_detail.html', {'product': product})
+
+def delete_product(request, id):
+    product = get_object_or_404(Product, id=id) 
+    product.delete()
+    return redirect('all_products')
+
+def update_product(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        image = request.POST.get("image")
+        unit = request.POST.get("unit")
+        amount = request.POST.get("amount")
+        
+        product.name = name
+        product.image = image
+        product.unit = unit
+        product.amount = amount
+        
+        product.save()
+        return redirect('product_detail', id)
+        
+        
+    elif request.method == 'GET':
+        return render(request, 'product_update.html', {'product': product})
+    return redirect('all_products')
+    
+
+        
